fc27ff1175a87bc34b187fbf4e7b2926a5315315
lakkaru <lakkaru@gmail.com>
2024-07-22 06:14:32 +0530

    gh-pages configured

---
+++ src/layouts/HomeLayout.js
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function HomeLayout() {
+  return (
+    <Box>
+      top nav <br />
+      slider <br />
+      services section <br />
+      visit store button <br />
+      coming soon section <br />
+      social section <br />
+      footer
+    </Box>
+  );
+}
